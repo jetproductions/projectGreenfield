2a78b3aef982500f4750e8334f41d1b01f8e4b6d
et <eric.thomson13@gmail.com>
2020-01-25 15:24:25 -0700

    Added id and mapState to QuesionAnswer

--- src/components/question-answer/QuestionAnswer.js
+++ src/components/question-answer/QuestionAnswer.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-const QuestionAnswer = (props) => (
-  <div>
-    QUESTIONS & ANSWERS
-  </div>
-);
+const QuestionAnswer = (props) => {
+  const { id } = useParams();
+  return (
+    <div>
+      QUESTIONS & ANSWERS
+    </div>
+  );
+};
 
-export default QuestionAnswer;
+const mapStateToProps = (state) => ({
+  productStore: state.product,
+});
+
+export default connect(mapStateToProps)(QuestionAnswer);
