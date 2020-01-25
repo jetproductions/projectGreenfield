618b115bc61d779e87238590f54d08c503cfd953
et <eric.thomson13@gmail.com>
2020-01-25 12:23:35 -0700

    Added ChildComponents

--- src/components/question-answer/QuestionAnswer.js
+++ src/components/question-answer/QuestionAnswer.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const QuestionAnswer = (props) => (
   <div>
-      Question Answer
+    QUESTIONS & ANSWERS
   </div>
 );
 
