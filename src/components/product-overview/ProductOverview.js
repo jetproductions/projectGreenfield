fc71000860a350bb8a5d3146f8816e3dffdd2247
James Kelley <james.a.kelley.mars@gmail.com>
2020-01-25 15:42:09 -0700

    Create basic comment-skeleton for product overview component

--- src/components/product-overview/ProductOverview.js
+++ src/components/product-overview/ProductOverview.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
+//  Has four sub-components
+  //  Image Gallery
+  //  Product Information
+  //  Style Selector
+  //  Add to Cart
+
 const ProductOverview = (props) => (
   <div>
       Product Overview
+      {/* Product View Class */}
+        {/* Left Column Grid */}
+          {/* Image Carousel(Gallery) */}
+        {/* Right Column Grid */}
+          {/* Product information */}
+            {/* Star Utility Component */}
+          {/* Style Selector */}
+          {/* Add to Cart */}
+
   </div>
 );
 
