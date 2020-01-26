4bb9f834b3a8c1152c46aa441d3eea2cafd830a1
tthenne <troy@hashinteractive.com>
2020-01-26 10:50:09 -0700

    refactored Product.js to only fetch product from api if route param id !== productStore.id

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,40 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import reviewsMetaData from '../../../sampleData/reviews/reviewMetaData';
 import './reviews.css';
 
-const Reviews = ({ product: { id } }) => {
-  const [reviewsMeta, setReviewsMeta] = useState({});
-  const [ratings, setRatings] = useState({});
-  const [totalRatings, setTotalRatings] = useState(0);
-  /* eslint-disable no-undef */
-  fetch(`http://3.134.102.30/reviews/${id}/meta`)
-    .then((res) => res.json())
-    .then((results) => {
-      setReviewsMeta(results);
-    });
+class Reviews extends Component {
+  constructor(props) {
+    super(props);
+    const { product: { id } } = props;
 
-  useEffect(() => {
-    const { ratings: allRatings = [] } = reviewsMeta;
-    const total = Object.values(allRatings).reduce((acc, curr) => acc + curr, 0);
-    setRatings(allRatings);
-    setTotalRatings(total);
-  }, ['reviewsMeta']);
+    this.state = {
+      reviewsMeta: reviewsMetaData,
+    };
+    this.getReviewsMeta(id);
+  }
 
-  return (
-    <div className="reviews py-12">
-      <div className="container mx-auto px-4">
-        <div className="flex w-full -mx-4">
-          <div className="w-full md:w-1/3 px-4">
-            <h5 className="uppercase font-thin text-lg">Ratings & Reviews</h5>
-          </div>
-          <div className="w-full md:w-2/3 px-4">
-            { totalRatings }
+  componentDidUpdate() {
+    const { product: { id } } = this.props;
+    this.getReviewsMeta(id);
+  }
+
+  getReviewsMeta = async (id) => {
+    /* eslint-disable no-undef */
+    const reviewsMeta = await fetch(`http://3.134.102.30/reviews/${id}/meta`).then((res) => res.json());
+    this.setState({ reviewsMeta });
+  }
+
+  getTotalRatings = (ratings) => Object.values(ratings).reduce((acc, value) => acc + value, 0)
+
+  render() {
+    const { reviewsMeta } = this.state;
+    const { ratings } = reviewsMeta;
+    const totalRatings = this.getTotalRatings(ratings);
+
+    return (
+      <div className="reviews py-12">
+        <div className="container mx-auto px-4">
+          <div className="flex w-full -mx-4">
+            <div className="w-full md:w-1/3 px-4">
+              <h5 className="uppercase font-thin text-lg">Ratings & Reviews</h5>
+            </div>
+            <div className="w-full md:w-2/3 px-4">
+              { totalRatings }
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapPropsToState = (state) => ({
   product: state.product,
