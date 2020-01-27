94e2394a1af626825b5007fa09b28692b243c2b1
et <eric.thomson13@gmail.com>
2020-01-27 10:51:55 -0700

    Rendering Questions

--- src/components/question-answer/Question.js
+++ src/components/question-answer/Question.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 import Answers from './Answers';
 
 const Question = ({
-  question_id, question_body, asker_name, question_helpfulness, answers,
+  question_body, asker_name, question_helpfulness, answers,
 }) => {
   if (question_body !== undefined) {
     return (
       <div>
-        <h4>{question_body}</h4>
+        <h4>
+Q:
+          {' '}
+          {question_body}
+        </h4>
       </div>
+			<Answers {...answers} />
     );
   }
   return (
