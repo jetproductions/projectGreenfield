f9364d272375466d17c9616e11d3e86746ae90b8
et <eric.thomson13@gmail.com>
2020-01-27 09:46:28 -0700

    Added GetQuestions to QuestionAnswer

--- src/components/question-answer/Questions.js
+++ src/components/question-answer/Questions.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Question from './Question';
 
-const Questions = ({ questions }) => (
-  questions.map((question) => {
-    const { id } = question;
-    return (
-      <Question key={id} />
-    );
-  })
-);
+const Questions = ({ questions }) => questions.map((question) => {
+  const { id } = question;
+  return (
+    <Question key={id} />
+  );
+});
 
 export default Questions;
