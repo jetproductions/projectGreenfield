668024b8af1630d0199c5a48f48d0bc0d405b207
et <eric.thomson13@gmail.com>
2020-01-27 09:27:32 -0700

    Map All Questions Added

--- src/components/question-answer/Questions.js
+++ src/components/question-answer/Questions.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import Question from './Question';
 
-const Questions = (props) => (
-  <div />
+const Questions = ({ questions }) => (
+  questions.map((question) => {
+    const { id } = question;
+    return (
+      <Question key={id} />
+    );
+  })
 );
 
 export default Questions;
