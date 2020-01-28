96ec91d906a65a9084a9b9b8c4e27f24aed31028
et <eric.thomson13@gmail.com>
2020-01-28 11:47:27 -0700

    Date String Formatted to Match Reviews Date String

--- src/components/question-answer/Answers/Answer.js
+++ src/components/question-answer/Answers/Answer.js
@@ -1,26 +1,30 @@
 /* eslint-disable camelcase */
 import React from 'react';
+import moment from 'moment';
 
 const Answer = ({
   answerer_name, body, helpfulness, date, photos,
-}) => (
-  <div>
-    <h2>
+}) => {
+  const dateString = moment(date).format('MMMM Do, YYYY');
+  return (
+    <div>
+      <h2>
 A:
-      {' '}
-      {body}
-    </h2>
-    <span>
-      {`By ${answerer_name}, ${date} | Helpful? Yes ${helpfulness} | Report`}
-    </span>
-    {photos.length > 0 ? (
-      <>
-        <h6>Photos:</h6>
-        <span>Photos Go Here</span>
-      </>
-    ) : null}
+        {' '}
+        {body}
+      </h2>
+      <span>
+        {`By ${answerer_name}, ${dateString} | Helpful? Yes ${helpfulness} | Report`}
+      </span>
+      {photos.length > 0 ? (
+        <>
+          <h6>Photos:</h6>
+          <span>Photos Go Here</span>
+        </>
+      ) : null}
 
-  </div>
-);
+    </div>
+  );
+};
 
 export default Answer;
