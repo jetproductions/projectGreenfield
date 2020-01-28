b9e812384815e739c44bc893a147bf6e9138ea23
et <eric.thomson13@gmail.com>
2020-01-28 14:58:34 -0700

    Updated Button Utility Style and Overall QA

--- src/components/utility/Button.js
+++ src/components/utility/Button.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Button = ({ name, submitHandler }) => {
-  return (
-    <button type="submit" onClick={(e) => submitHandler(e)}>{ name }</button>
-  );
-};
+const Button = ({ name, submitHandler }) => (
+  <button
+    onClick={(e) => submitHandler(e)}
+    type="button"
+    className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
+  >
+    { name }
+  </button>
+);
 
 export default Button;
