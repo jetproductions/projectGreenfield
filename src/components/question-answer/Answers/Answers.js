98b027f72485de03108d6624e8d172f8de0fb380
et <eric.thomson13@gmail.com>
2020-01-28 12:08:15 -0700

    Added Answers Count Variable Working on Show More Button

--- src/components/question-answer/Answers/Answers.js
+++ src/components/question-answer/Answers/Answers.js
@@ -4,6 +4,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Answer from './Answer';
+import Button from '../../utility/Button';
 
 // eslint-disable-next-line no-undef
 const getAnswers = (questionId) => fetch(`http://3.134.102.30/qa/${questionId}/answers`).then((res) => res.json());
@@ -17,20 +18,22 @@ const sortHelpfulness = (questionsArr) => questionsArr.sort((a, b) => {
 const Answers = ({ question_id }) => {
   const [answers, setAnswers] = useState([]);
   const [questionId, setQuestionId] = useState('');
+  const [numAnswers, setNumAnswers] = useState(0);
+  const addMoreButton = answers.length === numAnswers ? null : <Button name="Show More Answers" />;
 
   if (question_id !== questionId) {
-    getAnswers(question_id).then((result) => setAnswers(result.results));
+    getAnswers(question_id).then((result) => { setAnswers(result.results); setNumAnswers(parseInt(result.count, 10)); });
     setQuestionId(question_id);
   }
   if (answers.length > 0) {
-    console.log('answers.results ', answers.results);
-
-    return sortHelpfulness(answers).map((answer) => {
+    console.log(`numAnswers: ${numAnswers}`);
+    return (sortHelpfulness(answers).map((answer) => {
       const aid = answer.answer_id ? answer.answer_id : null;
       return (
         <Answer key={aid} {...answer} />
       );
-    });
+    })
+    );
   }
   return (<div />);
 };
--- src/components/utility/Button.js
+++ src/components/utility/Button.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Button = ({ name, submitHandler }) => {
-  return (
-    <button type="submit" onClick={(e) => submitHandler(e)}>{ name }</button>
-  );
-};
+const Button = ({ name, submitHandler }) => (
+  <button type="submit" onClick={(e) => submitHandler(e)}>{ name }</button>
+);
 
 export default Button;
