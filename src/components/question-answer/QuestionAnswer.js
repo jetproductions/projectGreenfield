6d98278f668fb6fbf05b457a66b9c7c5bde2358f
et <eric.thomson13@gmail.com>
2020-01-28 15:54:56 -0700

    Working on Changing QA to Class Component to Handle State For SubComponents

--- src/components/question-answer/QuestionAnswer.js
+++ src/components/question-answer/QuestionAnswer.js
@@ -1,18 +1,67 @@
-import React, { useState } from 'react';
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
+/* eslint-disable no-undef */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import SearchBar from './SearchBar';
 import Questions from './Questions';
+import Modal from '../utility/Modal';
+
+class QuestionAnswer extends Component {
+  constructor(props) {
+    super(props);
+    const { product: { id } } = props;
+    this.state = {
+      questions: [],
+      questionModal: false,
+      answerModal: false,
+      searched: '',
+    };
+    this.getQuestions(id);
+  }
 
 
-const QuestionAnswer = () => (
+  componentDidUpdate(prevProps) {
+	  const { product: { id } } = this.props;
+	  // eslint-disable-next-line no-useless-return
+	  if (id === prevProps.product.id) return;
+    // need to get new questions
+    this.getQuestions(id);
+  }
+
+  getQuestions = async (id) => {
+    const fetchQuestions = fetch(`http://3.134.102.30/qa/${id}`).then((res) => res.json());
+    const { results } = fetchQuestions;
+    this.setState({ questions: results });
+  }
+
+  // need to update Search to handle this and to pass back up upon search
+  // this might also need to handle filter for what questions get sent down?
+  onSearchChange = (e) => {
+    this.setState({ searched: e.target.value });
+  }
+
+  render() {
+	  const { questions } = this.state;
+	  const { answerModal } = this.state;
+	  const { questionModal } = this.state;
+	  return (
   <div>
     <div className="w-full px-4">
       <h5 className="uppercase font-thin text-xl">QUESTIONS & ANSWERS</h5>
     </div>
     <div>
-      <SearchBar />
-      <Questions />
+      <SearchBar onChangeHandler={this.onSearchChange} />
+      <Questions questions={questions} answerModal={answerModal} questionModal={questionModal} />
     </div>
   </div>
-);
+	  );
+  }
+}
 
-export default QuestionAnswer;
+const mapStateToProps = (state) => ({
+  product: state.product,
+});
+
+
+export default connect(mapStateToProps)(QuestionAnswer);
--- src/components/question-answer/Questions.js
+++ src/components/question-answer/Questions.js
@@ -6,19 +6,9 @@ import { useParams } from 'react-router-dom';
 
 import Question from './Question';
 
-const getQuestions = (id) => fetch(`http://3.134.102.30/qa/${id}`).then((res) => res.json());
-
-const Questions = ({ productStore }) => {
-  const { id } = useParams();
-  const [questions, setQuestions] = useState([]);
+const Questions = ({ questions }) => {
   // looking to see if product has changed or if startup and nothing in questions
   // eslint-disable-next-line no-undef
-  if (Number(id) !== Number(productStore.id) || questions.length === 0) {
-    getQuestions(id).then((result) => {
-      // update the questions in state
-      setQuestions(result.results);
-    });
-  }
   if (questions.length > 0) {
     return questions.map((question) => {
       const qid = question.question_id ? question.question_id : null;
@@ -34,8 +24,5 @@ const Questions = ({ productStore }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  productStore: state.product,
-});
 
-export default connect(mapStateToProps)(Questions);
+export default Questions;
