e1045f662aad9d37d22330785c7fc3f755d7694a
et <eric.thomson13@gmail.com>
2020-01-28 11:29:54 -0700

    Answers with Name and Date Rendering

--- src/components/question-answer/Answers/Answer.js
+++ src/components/question-answer/Answers/Answer.js
@@ -1,7 +1,31 @@
+/* eslint-disable camelcase */
 import React from 'react';
 
-const Answer = (props) => (
-  <div>Answer Here</div>
+const Answer = ({
+  answerer_name, body, helpfulness, date,
+}) => (
+  <div>
+    <h2>
+A:
+      {' '}
+      {body}
+    </h2>
+    <span>
+By
+      {`${answerer_name}, `}
+      {date}
+    </span>
+    <h6>Photos:</h6>
+    <spa>Photos Go Here</spa>
+  </div>
 );
 
 export default Answer;
+// {
+// 	"answer_id": 124960,
+// 	"body": "we don't like it",
+// 	"date": "2019-12-21T00:00:00.000Z",
+// 	"answerer_name": "ewe",
+// 	"helpfulness": 0,
+// 	"photos": []
+// },
