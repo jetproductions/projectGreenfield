b2f76cb284daba3eb72de594f02f348ecccced44
et <eric.thomson13@gmail.com>
2020-01-28 12:27:40 -0700

    Updated Styling for Byline to Match Reviews

--- src/components/question-answer/Answers/Answer.js
+++ src/components/question-answer/Answers/Answer.js
@@ -13,14 +13,20 @@ A:
         {' '}
         {body}
       </h2>
-      <span>
-        {`By ${answerer_name}, ${dateString} | Helpful? Yes ${helpfulness} | Report`}
-      </span>
+      <div className="flex items-center justify-end text-gray-700">
+        <small className="bg-gray-400 rounded-full mr-2 font-black h-auto" style={{ fontSize: '8px', padding: '2px 3.5px' }}>{String.fromCharCode(10003)}</small>
+        <span>{`By ${answerer_name}, `}</span>
+        <span className="ml-1">
+          { dateString }
+          {' '}
+          {`| Helpful? Yes ${helpfulness} | Report`}
+        </span>
+      </div>
       {photos.length > 0 ? (
-        <>
-          <h6>Photos:</h6>
+        <footer>
+          <div>Photos:</div>
           <span>Photos Go Here</span>
-        </>
+        </footer>
       ) : null}
 
     </div>
