fa583095f61cb0a19c083b81f71aeadeeaa8cd59
tthenne <troy@hashinteractive.com>
2020-01-28 19:49:07 -0700

    working on reviews form

--- src/components/reviews/form/ReviewForm.js
+++ src/components/reviews/form/ReviewForm.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Star from '../../utility/stars/Star';
 
 /* eslint-disable react/no-unused-state */
+/* eslint-disable react/no-array-index-key */
 class ReviewForm extends Component {
+  ratings = ['Poor', 'Fair', 'Average', 'Good', 'Great'];
+
   constructor(props) {
     super(props);
+    const stars = [...new Array(5)].map((e) => 0);
     this.state = {
       form: {
         username: '',
+        stars,
       },
     };
   }
 
+  fillStars = (index) => {
+    const { form, form: { stars } } = this.state;
+    const next = index + 1;
+    const filled = [...new Array(5)].map((e, i) => (i < next ? 1 : 0));
+    this.setState({ form: { ...form, stars: filled } });
+  }
+
   render = () => {
     const { product: { name } } = this.props;
+    const { form: { stars } } = this.state;
+    const ratingIndex = stars.lastIndexOf(1);
     return (
       <form className="w-full">
         <div className="flex flex-wrap -mx-4 mb-6">
@@ -23,6 +38,29 @@ class ReviewForm extends Component {
               About
               {` ${name}`}
             </h5>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-4 mb-6">
+          <div className="w-full items-center flex px-4">
+            <div className="flex mr-6">
+              {
+              stars.map((percent, i) => (
+                <a
+                  onClick={(e) => {
+                    e.preventDefault();
+                    this.fillStars(i);
+                  }}
+                  key={i}
+                  href="/"
+                >
+                  <Star size={30} percent={percent} />
+                </a>
+              ))
+            }
+            </div>
+            <div className="text-xl">
+              { this.ratings[ratingIndex] }
+            </div>
           </div>
         </div>
       </form>
