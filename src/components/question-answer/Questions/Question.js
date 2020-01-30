e51b424a4b4f2c5639a1f314fe996c1d3e3e0669
et <eric.thomson13@gmail.com>
2020-01-30 11:55:43 -0700

    Question HelpfulnessHandler Added

--- src/components/question-answer/Questions/Question.js
+++ src/components/question-answer/Questions/Question.js
@@ -3,15 +3,24 @@
 import React, { useState } from 'react';
 import Answers from '../Answers/Answers';
 
-const helpfulUpdate = (e, id) => {
+const helpfulUpdate = async (e, id) => {
   e.preventDefault();
   // eslint-disable-next-line no-undef
-  fetch(`http://3.134.102.30/qa/question/${id}/helpful`, { method: 'PUT', headers: { 'Content-Type': 'application/json' } }).then((result) => { console.log(result); });
+  const status = await fetch(`http://3.134.102.30/qa/question/${id}/helpful`, { method: 'PUT', headers: { 'Content-Type': 'application/json' } }).then((result) => result.status);
+  return status === 204;
 };
 const Question = ({
   question_id, question_body, asker_name, question_helpfulness,
 }) => {
   const [helpfulButton, buttonUsed] = useState(false);
+  const [helpfulnessState, helpfulnessUpdate] = useState(question_helpfulness);
+  const helpfulnessHander = async (e) => {
+    const updated = await helpfulUpdate(e, question_id);
+    if (updated) {
+      helpfulnessUpdate(helpfulnessState + 1);
+      buttonUsed(true);
+    }
+  };
   return (
     <div>
       <h4>
@@ -21,9 +30,9 @@ Q:
       </h4>
       <span>
 Helpfulness?
-        <button type="button" disabled={helpfulButton} onClick={(e) => { buttonUsed(true); helpfulUpdate(e, question_id); }}>Yes</button>
+        <button type="button" disabled={helpfulButton} onClick={(e) => { helpfulnessHander(e); }}>Yes</button>
       (
-        {question_helpfulness}
+        {helpfulnessState}
 )
       </span>
       <Answers question_id={question_id || null} />
