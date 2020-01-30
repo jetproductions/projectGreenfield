cbb30bb7361cfc224c3fa9f386480db9851cc16a
et <eric.thomson13@gmail.com>
2020-01-30 11:20:26 -0700

    Helpfulness in Questions Updating Working on Only Usable Once

--- src/components/question-answer/Questions/Question.js
+++ src/components/question-answer/Questions/Question.js
@@ -3,6 +3,11 @@
 import React from 'react';
 import Answers from '../Answers/Answers';
 
+const helpfulUpdate = (e, id) => {
+  e.preventDefault();
+  // eslint-disable-next-line no-undef
+  fetch(`http://3.134.102.30/qa/question/${id}/helpful`, { method: 'PUT', headers: { 'Content-Type': 'application/json' } }).then((result) => { console.log(result); });
+};
 const Question = ({
   question_id, question_body, asker_name, question_helpfulness,
 }) => (
@@ -14,7 +19,10 @@ Q:
     </h4>
     <span>
 Helpfulness?
+      <button onClick={(e) => { helpfulUpdate(e, question_id); }}>Yes</button>
+      '('
       {question_helpfulness}
+')'
     </span>
     <Answers question_id={question_id || null} />
 
