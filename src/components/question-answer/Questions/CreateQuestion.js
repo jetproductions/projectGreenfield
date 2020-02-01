8048f318c84b9b86b6232ad3f406cce1d7cbb83a
et <eric.thomson13@gmail.com>
2020-02-01 08:00:49 -0700

    Create Question Posting but not Updating After Sent

--- src/components/question-answer/Questions/CreateQuestion.js
+++ src/components/question-answer/Questions/CreateQuestion.js
@@ -49,7 +49,8 @@ class CreateQuestion extends Component {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
-      }).then((res) => res.json());
+      });
+    // .then((res) => res.json());
     console.log('created: ', created.status);
     return created.status === 201;
   }
