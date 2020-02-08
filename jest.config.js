cc59b0f9da5e1d4d50c1497993b5326628c05c24
et <eric.thomson13@gmail.com>
2020-02-07 18:51:51 -0700

    Button Test Failing but Not Because JSX

--- jest.config.js
+++ jest.config.js
@@ -30,4 +30,5 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  verbose: true,
 };
--- test/Button.test.js
+++ test/Button.test.js
@@ -4,9 +4,8 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Button from '../src/components/utility/Button';
 
-
-const renderer = TestRenderer.ReactTestRenderer;
-const instance = TestRenderer.ReactTestInstance;
 describe('<Button /> Testing', () => {
-  it('renders Button without crashing', () => shallow(<Button />));
+  it('renders Button without crashing', () => {
+    shallow(<Button />)
+  });
 });
